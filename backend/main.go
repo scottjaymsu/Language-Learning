5e50b13f88d60a7f927616baea29dc2fcb3f76d3
Jay <scottjay@msu.edu>
2024-04-08 02:49:05 -0400

    backend setup

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,12 @@
-package backend
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+	http.ListenAndServe(":8080", nil)
+}
